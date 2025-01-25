31240e31e0500ba1018819bbcd959c62c1a1e435
vishnu-3001 <vishnusai3001@yahoo.com>
2025-01-25 18:44:55 -0500

    postgres database creation and connection setup

--- Project/app/__init__.py
+++ Project/app/__init__.py
@@ -1,5 +1,18 @@
 from fastapi import FastAPI
+from .database import init_db, close_pool
+from contextlib import asynccontextmanager
 
-app = FastAPI(title="VIP-testing api", description="APIs for VIP project using FastAPI and Langchain", version="1.0")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Setup: Initialize database connection pool
+    init_db()
+    print("Database connection pool initialized.")
+    yield
+    # Teardown: Close the connection pool
+    close_pool()
+    print("Database connection pool closed.")
+
+app = FastAPI(title="VIP-testing api", description="APIs for VIP project using FastAPI and Langchain", version="1.0",lifespan=lifespan)
 
 __all__ = ["app"]
